8e9e32140acd37a38835a500229327d9c5aa55c4
parashuramb37 <152781100+parashuramb37@users.noreply.github.com>
2023-12-03 16:42:32 +0530

    Python program to get accelerometer data of an android device
    Output will be displayed in XYZ co-ordinates

---
+++ main.py
@@ -0,0 +1,35 @@
+import websocket
+import json
+
+
+def on_message(ws, message):
+    values = json.loads(message)['values']
+    x = values[0]
+    y = values[1]
+    z = values[2]
+    print("x = ", x, "y = ", y, "z = ", z)
+
+
+def on_error(ws, error):
+    print("error occurred ", error)
+
+
+def on_close(ws, close_code, reason):
+    print("connection closed : ", reason)
+
+
+def on_open(ws):
+    print("connected")
+
+
+def connect(url):
+    ws = websocket.WebSocketApp(url,
+                                on_open=on_open,
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close)
+
+    ws.run_forever()
+
+
+connect("ws://192.168.1.4:8080/sensor/connect?type=android.sensor.accelerometer")
